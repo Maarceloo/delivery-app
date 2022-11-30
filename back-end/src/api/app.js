09ed03eac5b0879b49b74037db860e4bf1010579
Matheus leão <kyracraft0@gmail.com>
2022-11-30 15:42:46 -0300

    Criada rota de get Users

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const userControllers = require('./Controllers/UserControllers')
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.get('/login', userControllers.login)
 
 module.exports = app;
