527019991eb906854193c6887bcd7f646f655361
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-01 16:02:19 -0300

    feat: implementando token em user

--- back-end/src/api/Controllers/UserControllers.js
+++ back-end/src/api/Controllers/UserControllers.js
@@ -1,24 +1,30 @@
 const userServices = require('../Services/UserServices');
+const { jwtSign } = require('../Utils/Jwt');
 
 const login = async (req, res) => {
-    const { body } = req;
-    const { status, message } = await userServices.login(body);
-    if (status) {
-        return res.status(status).json(message);
-    }
-    return res.status(200).json(message);
+  const { body } = req;
+  const { status, message } = await userServices.login(body);
+
+  if (status) {
+    return res.status(status).json(message);
+  }
+
+  const token = await jwtSign(message);
+  const { name, email, role } = message;
+
+  return res.status(200).json({ name, email, role, token });
 };
 
 const postUser = async (req, res) => {
-    const { body } = req;
-    const { status, message } = await userServices.postUser(body);
-    if (status) {
-        return res.status(status).json(message);
-    }
-    return res.status(201).json(message);
+  const { body } = req;
+  const { status, message } = await userServices.postUser(body);
+  if (status) {
+    return res.status(status).json(message);
+  }
+  return res.status(201).json(message);
 };
 
 module.exports = {
-    login,
-    postUser,
+  login,
+  postUser,
 };
