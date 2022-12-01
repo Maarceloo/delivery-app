c80eaa223edd5ad1191f521644866deaa342ff8e
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-01 16:03:06 -0300

    fix

--- back-end/src/database/models/ProductsModel.js
+++ back-end/src/database/models/ProductsModel.js
@@ -1,6 +1,6 @@
 const ProducstModelSchema = (sequelize, DataTypes) => {
     const ProductsModel = sequelize.define(
-      "products",
+      "Product",
       {
         id: {
           primaryKey: true, 
