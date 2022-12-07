02e0adc0aadf41b7fa22c9cd46d0cf8522b057a7
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-07 18:43:37 -0300

    feat: rota implementada

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const userRoutes = require('./Routes/UserRoutes');
 const productRoutes = require('./Routes/ProductsRoutes');
 const salesRoute = require('./Routes/SalesRoutes');
+const salesProductsRoute = require('./Routes/SalesProductsRoutes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(userRoutes);
 app.use(express.static('public'));
 app.use(productRoutes);
 app.use(salesRoute);
+app.use(salesProductsRoute);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
