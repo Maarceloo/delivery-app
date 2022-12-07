324a2fd17d5cc747f82e2b999a06e14180f70503
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-07 19:26:48 -0300

    fix: References impedia a criacao da tabela

--- back-end/src/database/migrations/04-sales-products.js
+++ back-end/src/database/migrations/04-sales-products.js
@@ -5,10 +5,10 @@ module.exports = {
             allowNull: false,
             primaryKey: true,
             type: Sequelize.INTEGER,
-            references: {
-                model: 'Sales',
-                key: 'id'
-            },
+            // references: {
+            //     model: 'Sales',
+            //     key: 'id'
+            // },
             onUpdate: 'CASCADE',
             onDelete: 'CASCADE'
         },
@@ -16,10 +16,10 @@ module.exports = {
             allowNull: false,
             primaryKey: true,
             type: Sequelize.INTEGER, 
-            references: {
-                model: 'Product',
-                key: 'id'
-            },
+            // references: {
+            //     model: 'Product',
+            //     key: 'id'
+            // },
             onUpdate: 'CASCADE',
             onDelete: 'CASCADE'
         },
