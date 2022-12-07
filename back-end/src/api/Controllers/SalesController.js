e1ee0371a13530c57e7b8fa16d5cb0365b5368a2
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-07 15:39:08 -0300

    feat: controller de sales implementado

--- back-end/src/api/Controllers/SalesController.js
+++ back-end/src/api/Controllers/SalesController.js
@@ -1,8 +1,14 @@
 const service = require('../Services/SalesService');
 
 const postSales = async (req, res) => {
-  const data = await service.postSales(req.body);
+  const idSale = await service.postSales(req.body);
+  const newSale = await service.getIdSale(idSale);
+  return res.status(201).json(newSale);
+};
+
+const getAllSales = async (_req, res) => {
+  const data = await service.getAllSales();
   return res.status(200).json(data);
 };
 
-module.exports = { postSales };
+module.exports = { postSales, getAllSales };
