61616d822c72030f12bf4a54b4031014df33fd09
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-12-08 15:10:51 -0300

    Inicio da pagina de checkout

--- front-end/src/Pages/Checkout.jsx
+++ front-end/src/Pages/Checkout.jsx
@@ -5,6 +5,7 @@ function Checkout() {
   return (
     <div>
       <NavBar />
+      <h1>Checkout</h1>
       <table>
         <thead>
           <tr>
@@ -43,11 +44,54 @@ function Checkout() {
             >
               R$ 50,00
             </td>
+            <button
+              type="button"
+              data-testid="customer_checkout__element-order-table-remove-<index>"
+              value="Remover"
+            >
+              Remover
+            </button>
           </tr>
-
         </tbody>
       </table>
-      <h1>Checkout</h1>
+      <h2>Total da compra: R$</h2>
+      <h1>Detalhes e Endereço para Entrega</h1>
+      <form>
+        <label htmlFor="seller">
+          P.Vendedora Responsável
+          <select
+            data-testid="customer_checkout__select-seller"
+            name="seller"
+            id="seller"
+          >
+            <option value="1">Vendedora 1</option>
+          </select>
+        </label>
+        <label htmlFor="address">
+          Endereço de Entrega
+          <input
+            type="text"
+            data-testid="customer_checkout__input-address"
+            name="address"
+            id="address"
+          />
+        </label>
+        <label htmlFor="number">
+          Número
+          <input
+            type="text"
+            data-testid="customer_checkout__input-addressNumber"
+            name="number"
+            id="number"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="customer_checkout__button-submit-order"
+        >
+          Finalizar Pedido
+        </button>
+      </form>
     </div>
   );
 }
