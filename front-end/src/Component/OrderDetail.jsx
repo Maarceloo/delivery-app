f130f9ac8ae53e0dadabfab8cc7902fd5909df49
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-13 16:48:05 -0300

    feat: desenvolvendo orderDetails

--- front-end/src/Component/OrderDetail.jsx
+++ front-end/src/Component/OrderDetail.jsx
@@ -4,31 +4,43 @@ import { getData } from '../Service/request';
 
 function OrderDetail() {
   const { id } = useParams();
-  // const [getId] = useState(id);
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
-    console.log(id);
-    const data = await getData('sales/products');
-    console.log(products);
+    const data = await getData(`sales/products/${id}`);
     setProducts(data);
   }
+
+  const totalProducts = () => {
+    const copyCartProducts = [...products];
+    const total = copyCartProducts
+      .reduce((acc, curr) => acc + curr.quantity * curr.products.price, 0);
+    return total.toFixed(2);
+  };
 
   useEffect(() => {
     getProducts();
   }, []);
 
+  const dataTestid = 'customer_order_details__element-order-table-';
+  const dataTestid2 = 'customer_order_details__element-order-details-label-';
+
   return (
     <>
       <h1> Detalhe do pedido</h1>
       <section>
-        <h1>numero do pedido</h1>
-        <h1> nome do vendedor</h1>
-        <h1>data</h1>
-        <h1>status do pedido</h1>
-        <button type="button">pedido entregue</button>
+        <h1 data-testid={ `${dataTestid2}order-id` }>numero do pedido</h1>
+        <h1 data-testid={ `${dataTestid2}seller-name` }> nome do vendedor</h1>
+        <h1 data-testid={ `${dataTestid2}order-date` }>data</h1>
+        <h1 data-testid={ `${dataTestid2}delivery-status` }>status do pedido</h1>
+        <button
+          data-testid="customer_order_details__button-delivery-check"
+          type="button"
+        >
+          pedido entregue
+        </button>
       </section>
-      {/* <table>
+      <table>
         <thead>
           <tr>
             <th>Item</th>
@@ -39,29 +51,32 @@ function OrderDetail() {
           </tr>
         </thead>
         <tbody>
-          {products.map((item, index) => (
-            <tr key={ item.id }>
-              <td data-testid={ `${dataTestid}item-number-${index}` }>
-                {index + 1 }
-              </td>
-              <td data-testid={ `${dataTestid}name-${index}` }>{item.name}</td>
-              <td data-testid={ `${dataTestid}quantity-${index}` }>
-                {item.quantity}
-              </td>
-              <td data-testid={ `${dataTestid}unit-price-${index}` }>
-                {item.price.replace(/\./, ',')}
-              </td>
-              <td data-testid={ `${dataTestid}sub-total-${index}` }>
-                {(item.price * item.quantity).toFixed(2).replace(/\./, ',')}
-              </td>
-            </tr>
-          ))}
+          {products
+            && products.map((item, index) => (
+              <tr key={ index }>
+                <td data-testid={ `${dataTestid}item-number-${index}` }>
+                  {index + 1}
+                </td>
+                <td data-testid={ `${dataTestid}name-${index}` }>
+                  {item.products.name}
+                </td>
+                <td data-testid={ `${dataTestid}quantity-${index}` }>
+                  {item.quantity}
+                </td>
+                <td data-testid={ `${dataTestid}unit-price-${index}` }>
+                  {item.products.price}
+                </td>
+                <td data-testid={ `${dataTestid}sub-total-${index}` }>
+                  {(item.products.price * item.quantity).toFixed(2).replace(/\./, ',')}
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
-      <h2 data-testid="customer_checkout__element-order-total-price">
+      <h2 data-testid="customer_order_details__element-order-total-price">
         Total da compra: R$
         {` ${totalProducts().replace(/\./, ',')} `}
-      </h2> */}
+      </h2>
     </>
   );
 }
