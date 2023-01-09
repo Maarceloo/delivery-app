37707ca8ff9bc4ce479fd18d1bb15dad9a46a598
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-01-09 19:34:42 -0300

    Finaliza tela de chekcout

--- front-end/src/Component/CheckoutCard.jsx
+++ front-end/src/Component/CheckoutCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-max-depth */
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getData, postData } from '../Service/request';
@@ -83,93 +84,114 @@ function CheckoutCard() {
   return (
     <div className="Main-Div-Checkout">
       <h1>Finalizar Pedido</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Descrição</th>
-            <th>Quantidade</th>
-            <th>Valor Unitário</th>
-            <th>Subtotal</th>
-            <th>Remover</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartProducts.map((item, index) => (
-            <tr key={ item.id }>
-              <td data-testid={ `${dataTestid}item-number-${index}` }>
-                {index + 1 }
-              </td>
-              <td data-testid={ `${dataTestid}name-${index}` }>{item.name}</td>
-              <td data-testid={ `${dataTestid}quantity-${index}` }>
-                {item.quantity}
-              </td>
-              <td data-testid={ `${dataTestid}unit-price-${index}` }>
-                {item.price.replace(/\./, ',')}
-              </td>
-              <td data-testid={ `${dataTestid}sub-total-${index}` }>
-                {(item.price * item.quantity).toFixed(2).replace(/\./, ',')}
-              </td>
-              <button
-                type="button"
-                data-testid={ `${dataTestid}remove-${index}` }
-                value="Remover"
-                onClick={ () => removeItemCart(item) }
-              >
-                Remover
-              </button>
+      <section className="Board-Checkout">
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor Unitário</th>
+              <th>Subtotal</th>
+              <th>Remover</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <h2 data-testid="customer_checkout__element-order-total-price">
-        Total da compra: R$
-        {` ${totalProducts().replace(/\./, ',')} `}
-      </h2>
-      <h1>Detalhes e Endereço para Entrega</h1>
-      <form>
-        <label htmlFor="seller">
-          P.Vendedora Responsável
-          <select
-            data-testid="customer_checkout__select-seller"
-            name="seller"
-            id="seller"
-            onChange={ ({ target }) => { saveSeller(target.value); } }
+          </thead>
+          <tbody>
+            {cartProducts.map((item, index) => (
+              <tr key={ item.id }>
+                <td data-testid={ `${dataTestid}item-number-${index}` }>
+                  {index + 1 }
+                </td>
+                <td data-testid={ `${dataTestid}name-${index}` }>{item.name}</td>
+                <td data-testid={ `${dataTestid}quantity-${index}` }>
+                  {item.quantity}
+                </td>
+                <td data-testid={ `${dataTestid}unit-price-${index}` }>
+                  {item.price.replace(/\./, ',')}
+                </td>
+                <td data-testid={ `${dataTestid}sub-total-${index}` }>
+                  {(item.price * item.quantity).toFixed(2).replace(/\./, ',')}
+                </td>
+                <button
+                  className="Btn-Remove"
+                  type="button"
+                  data-testid={ `${dataTestid}remove-${index}` }
+                  value="Remover"
+                  onClick={ () => removeItemCart(item) }
+                >
+                  Remover
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h2 data-testid="customer_checkout__element-order-total-price">
+          Total da compra: R$
+          {` ${totalProducts().replace(/\./, ',')} `}
+        </h2>
+      </section>
+      <form className="Board-Checkout-Payment">
+        <fieldset className="Legend-Checkout">
+          <legend
+            className="Legend-Checkout-H1"
+            align="center"
           >
-            { seller.map((sell) => (
-              <option key={ sell.id } value={ sell.id }>{sell.name}</option>
+            Detalhes e Endereço para Entrega
 
-            ), [])}
+          </legend>
+          <label htmlFor="seller">
+            P.Vendedora Responsável
+            <br />
+            <select
+              className="Input-Checkout"
+              data-testid="customer_checkout__select-seller"
+              name="seller"
+              id="seller"
+              onChange={ ({ target }) => { saveSeller(target.value); } }
+            >
+              { seller.map((sell) => (
+                <option key={ sell.id } value={ sell.id }>{sell.name}</option>
 
-          </select>
-        </label>
-        <label htmlFor="address">
-          Endereço de Entrega
-          <input
-            type="text"
-            data-testid="customer_checkout__input-address"
-            onChange={ ({ target }) => { saveAddress(target.value); } }
-            name="address"
-            id="address"
-          />
-        </label>
-        <label htmlFor="number">
-          Número
-          <input
-            type="text"
-            data-testid="customer_checkout__input-address-number"
-            onChange={ ({ target }) => { saveNumber(target.value); } }
-            name="number"
-            id="number"
-          />
-        </label>
-        <button
-          type="button"
-          onClick={ () => { postSales(); } }
-          data-testid="customer_checkout__button-submit-order"
-        >
-          Finalizar Pedido
-        </button>
+              ), [])}
+
+            </select>
+          </label>
+          <br />
+          <label htmlFor="address">
+            Endereço de Entrega
+            <br />
+            <input
+              className="Input-Checkout"
+              type="text"
+              data-testid="customer_checkout__input-address"
+              onChange={ ({ target }) => { saveAddress(target.value); } }
+              name="address"
+              id="address"
+            />
+          </label>
+          <br />
+          <label htmlFor="number">
+            Número
+            <br />
+            <input
+              className="Input-Checkout"
+              type="text"
+              data-testid="customer_checkout__input-address-number"
+              onChange={ ({ target }) => { saveNumber(target.value); } }
+              name="number"
+              id="number"
+            />
+          </label>
+          <br />
+          <button
+            className="Btn-Checkout"
+            type="button"
+            onClick={ () => { postSales(); } }
+            data-testid="customer_checkout__button-submit-order"
+          >
+            Finalizar Pedido
+          </button>
+        </fieldset>
       </form>
     </div>
   );
